59c8bca7b8eb7626a4181a8963e6117d8d45ce8a
Jackson Freitas <jacksonzitap.mc@gmail.com>
2023-06-04 10:28:19 -0300

    finishing page profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,30 +1,42 @@
-import { FiUser, FiMail, FiLock, FiCamera  } from 'react-icons/fi';
+import { FiUser, FiMail, FiLock, FiCamera, FiArrowLeft  } from 'react-icons/fi';
 import { ButtonText } from '../../components/ButtonText';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
-import { Container, Form } from './style';
+import { Container, Form, Avatar } from './style';
 
 export function Profile(){
     return(
      <Container>
         <header>
-            <ButtonText />
+            <ButtonText icon={FiArrowLeft} title="Voltar"/>
         </header>
             <main>
-            <div className='profile'>
-                <img src=''></img>
-                <div className='icon-camere'>
-                    {FiCamera}
-                </div>
-            </div>
-            <Form>
-                <Input icon={FiUser}  value="Jackson Freitas"/>
-                <Input icon={FiMail}  value="jacksonzitap.mc@gmail.com"/>
-                <Input icon={FiLock}  placeholder="Senha atual"/>
-                <Input icon={FiLock}  placeholder="Nova senha"/>
-                <Button title="Salvar"/>
-            </Form>
-        </main>
+                <Avatar>
+                    <img
+                        src="https://github.com/jacksonMarcelinoFreitas.png"
+                        alt="Foto do Usuário"
+                    />
+
+                    <label htmlFor="avatar">
+                        <FiCamera/>
+                        <input
+                            id="avatar"
+                            type="file"
+                        />
+                    </label>
+                </Avatar>
+                <Form>
+                    <div className="box-input">
+                        <Input type="text" icon={FiUser}  placeholder="Nome"/>
+                        <Input type="e-mail" icon={FiMail}  placeholder="jacksonzitap.mc@gmail.com"/>
+                    </div>
+                    <div className="box-input">
+                        <Input type="password" icon={FiLock}  placeholder="Senha atual"/>
+                        <Input type="password" icon={FiLock}  placeholder="Nova senha"/>
+                        <Button title="Salvar"/>
+                    </div>
+                </Form>
+            </main>
      </Container>
     )
 }
--- src/pages/Profile/style.js
+++ src/pages/Profile/style.js
@@ -1,19 +1,100 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: grid;
+  grid-template-rows: 114px 500px auto;
+  grid-template-areas:
+  "header"
+  "avatar"
+  "main";
 
+  > main{
+    height: 100%;
+    width: 100%;
+    grid-area: "main";
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 
+    padding: 0 144px;
+  }
 
-`
+  > header{
+    grid-area: "header";
+    display: flex;
+    align-items: center;
+    justify-content: start;
 
-export const Profle = styled.div`
+    width: 100%;
+    height: 144px;
+    padding: 0 144px;
 
+    background: ${({theme}) => theme.COLORS.PINK_50};
+  }
 
+`;
+
+export const Avatar = styled.div`
+  grid-area: "avatar";
+  position: relative;
+  width: 186px;
+  height: 186px;
+
+  > img{
+        border-radius: 50%;
+        width: 186px;
+        height: 186px;
+    }
+
+  > label{
+        width: 48px;
+        height: 48px;
+
+        background: ${({theme}) => theme.COLORS.PINK};
+
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        position: absolute;
+        bottom: 7px;
+        right: 7px;
+
+        cursor: pointer;
+
+        input{
+            display: none;
+        };
+
+        svg{
+            width: 20px;
+            height: 20px;
+            color: ${({theme}) => theme.COLORS.BACKGROUND_800};
+        }
+    }
+
+    svg{
+            width: 20px;
+            height: 20px;
+            color: ${({theme}) => theme.COLORS.BLACK};
+    }
 
 `
 
 export const Form = styled.form`
+  display: flex;
+  align-items:center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 24px;
 
+  margin-top: 64px;
 
+  width: 340px;
 
-`
+  > .box-input{
+    width: 100%;
+  }
+`;
